ee401822df8fe5d8e403cd5e617f3b445f1630bd
Joao-Inacio <joaoinacio206@protonmail.com>
2023-06-12 10:39:59 -0300

    renderizando os dados csv

--- app/views.py
+++ app/views.py
@@ -1,6 +1,19 @@
+# import numpy as np
+import pandas as pd
 from django.shortcuts import render
 
 
 # Create your views here.
 def home(request):
-    return render(request, 'index.html')
+    df = pd.read_csv(r'app\static\data\netflix_titles.csv')
+    data = {}
+
+    df['country'] = df['country'].apply(
+        lambda x: 'Brasil' if x == 'Brazil' else x
+        )
+
+    data['dados'] = df.to_html(
+        index=False,
+        classes=['table', 'table-striped', 'mt-5']
+        )
+    return render(request, 'index.html', data)
