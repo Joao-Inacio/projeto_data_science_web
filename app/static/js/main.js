270e7843add11d2f2281038e664c54e763cac78b
Joao-Inacio <joaoinacio206@protonmail.com>
2023-06-13 11:07:31 -0300

    Visualizando os dados escolhidos

---
+++ app/static/js/main.js
@@ -0,0 +1,21 @@
+(function(win,doc){
+    'use strict';
+
+    doc.querySelector('#country').addEventListener('change',async(event)=>{
+        event.preventDefault();
+        let req=await fetch('http://127.0.0.1:8000/countryFilter/',{
+            method:'POST',
+            headers:{
+                'Accept':'application/json',
+                'Content-Type':'application/json',
+                'X-CSRFToken':doc.querySelectorAll('input')[0].value
+            },
+            body:JSON.stringify({
+                'country':doc.querySelector('#country').value
+            })
+        });
+        let res=await req.json();
+        doc.querySelector('.result').innerHTML=res.data;
+    },false);
+
+})(window,document);
--- app/views.py
+++ app/views.py
@@ -1,19 +1,35 @@
 # import numpy as np
+import json
 import pandas as pd
+from django.http import JsonResponse
 from django.shortcuts import render
+
+df = pd.read_csv(r'app\static\data\netflix_titles.csv')
+data = {}
 
 
 # Create your views here.
 def home(request):
-    df = pd.read_csv(r'app\static\data\netflix_titles.csv')
-    data = {}
-
-    df['country'] = df['country'].apply(
-        lambda x: 'Brasil' if x == 'Brazil' else x
-        )
-
-    data['dados'] = df.to_html(
-        index=False,
-        classes=['table', 'table-striped', 'mt-5']
-        )
+    data['dados'] = df\
+        .drop(['show_id', 'date_added'], axis=1)\
+        .dropna()\
+        .head(20)\
+        .to_html(index=False, classes=['table', 'table-striped', 'mt-5'])
+    data['countryFilter'] = df['country'].sort_values().unique()
     return render(request, 'index.html', data)
+
+
+# Requisição para filtro de país
+from django.http import JsonResponse, HttpResponseBadRequest
+
+
+def countryFilter(request):
+    if request.body:
+        field = json.loads(request.body.decode('utf-8'))
+        search = field['country']
+        df2 = df.dropna()
+        data['dados'] = df2[df2['country'].str.contains(search)]\
+            .to_html(index=False, classes=['table', 'table-striped', 'mt-5'])
+        return JsonResponse({'data': data['dados']})
+    else:
+        return HttpResponseBadRequest('Dados inválidos')
